4daf531d3fa5e9c77f6105a4b975ae23f63cf91b
Brent Stone <brent.jk.stone@gmail.com>
2025-02-25 11:28:38 -0500

    Dask service added
    Dask scheduler, worker, and Jupyter Notebook services added. Begin refactor of services into compartmented compose and run scripts to minimize the computer and memory resources needed to run the containers

---
+++ run_dev_dask.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# stop execution instantly on non-zero status. This is to know location of error
+set -e
+
+export DOCKER_BUILDKIT=1
+
+# Networks and other thing from a previous run may still be around
+# This ensures a relatively clean slate
+docker compose -f docker-compose.avengercon-2025.dask.yaml down
+
+docker compose -f docker-compose.avengercon-2025.dask.yaml up --build
